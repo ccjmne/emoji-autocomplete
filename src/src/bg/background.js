f79b80636c9674f393199ce0eddf6c82c6b1a3b2
ccjmne <ccjmne@gmail.com>
2017-04-21 00:59:24 +0200

    Sort matched emoji

--- src/src/bg/background.js
+++ src/src/bg/background.js
@@ -13,9 +13,19 @@ window.chrome.extension.onMessage.addListener(function (request, sender, sendRes
   case 'config':
     return sendResponse(config);
   case 'query':
-    return sendResponse(_(emoji.lib).filter(emoji => {
-      return _.filter(emoji.keywords, keyword => keyword.indexOf(request.keyword) + 1).length;
-    }).take(8).value());
+    const query = request.keyword.toLowerCase();
+    return sendResponse(_(emoji.lib).filter((emoji, key) => {
+      emoji.__key = key;
+      const foundKey = key.indexOf(query);
+      if (foundKey !== -1) {
+        return (emoji.__priority = foundKey.toString() + key);
+      }
+
+      const foundKeyword = _(emoji.keywords).map(keyword => keyword.indexOf(query)).filter(idx => idx !== -1).reduce(Math.min);
+      if (typeof foundKeyword !== 'undefined') {
+        return (emoji.__priority = '_' + foundKeyword.toString());
+      }
+    }).sortBy('__priority').take(8).value());
   }
 
   return sendResponse();
